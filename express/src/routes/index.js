0d88acdfd5358db4886a4a843328e4bcf3d3108e
ThanhHM <thanhhm@runsystem.net>
2020-03-11 14:06:37 +0700

    Basic source Nodejs Express

---
+++ express/src/routes/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  // res.send('hello world')l
+  res.json({index:'setup exvironment test'})
+});
+
+export default router;
